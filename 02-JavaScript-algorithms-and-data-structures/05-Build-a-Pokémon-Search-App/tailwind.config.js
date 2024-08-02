b957929aeaf64d84798e4be8210dcc4fb9eff029
EMECarrellan <105559539+EMECarrellan@users.noreply.github.com>
2024-08-02 16:35:27 +0200

    font family import in tailwind

--- 02-JavaScript-algorithms-and-data-structures/05-Build-a-Pokémon-Search-App/tailwind.config.js
+++ 02-JavaScript-algorithms-and-data-structures/05-Build-a-Pokémon-Search-App/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+        display: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
